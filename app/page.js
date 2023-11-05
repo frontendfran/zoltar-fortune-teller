9ab1660840320887a802121f3d2071c92fd74117
frontendfran <hello@franhaines.com>
2023-11-05 19:55:33 +0000

    Install fonts

--- app/page.js
+++ app/page.js
@@ -1,12 +1,21 @@
+// https://nextjs.org/docs/pages/building-your-application/optimizing/fonts
+// https://fonts.google.com/specimen/Charm?preview.text=ZOLTAR&classification=Handwriting
+
 'use client'
 
-import { useEffect, useState } from "react";
+import { useEffect, useState } from 'react';
+import { Roboto } from 'next/font/google';
+
+const roboto = Roboto({
+  weight: '400',
+  subsets: ['latin'],
+})
 
 export default function Home() {
-  const [fortune, setFortune] = useState("");
+  const [fortune, setFortune] = useState('');
 
   async function getFortune() {
-    const res = await fetch("https://api.adviceslip.com/advice");
+    const res = await fetch('https://api.adviceslip.com/advice');
     const data = await res.json();
     setFortune(data.slip.advice);
   }
@@ -16,9 +25,9 @@ export default function Home() {
   }, []);
 
   return (
-    <div>
+    <main className={roboto.className}>
       <h1>{fortune}</h1>
       <button onClick={getFortune}>Get Fortune</button>
-    </div>
+    </main>
   );
 }
